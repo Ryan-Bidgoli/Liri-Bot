dc78288feb671240670994759781ceed702b0433
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-12-18 15:48:41 -0800

    adding node packages

---
+++ liri.js
@@ -0,0 +1,16 @@
+
+ 
+require("dotenv").config();
+ 
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
+var moment = require("moment");
+ 
+var fs = require("fs");
+var query = process.argv[3];
+
+ 
+var option = process.argv[2];
+ 
+var spotify = new Spotify(keys.spotify);
