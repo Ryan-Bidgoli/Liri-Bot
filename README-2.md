2f7b2742edbad9e79b5e46ac5817a2f82f9376cd
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-12-19 18:20:39 -0800

    rename screenshots

---
+++ README-2.md
@@ -0,0 +1,27 @@
+# Liri-Bot
+
+
+LIRI is a command line node app that takes in parameters and gives you back data based off the following parameters: 
+
+-	concert-this
+-	Spotify-this-song
+-	movie-this
+-	do-what-it-says
+
+## Screenshots:
+
+![default spotify song](screenshots/screenshot-01.png)
+![inputted spotify song with url](screenshots/screenshot-02.png)
+![inputted spotify song without url](screenshots/screenshot-03.png)
+![inputted artist for concert](screenshots/screenshot-04.png)
+![default movie](screenshots/screenshot-05.png)
+![inputted movie](screenshots/screenshot-06.png)
+
+## Technologies used:
+
+* Spotify API
+* OMDB API
+* Bands In Town API
+* Node.js
+* Javascript
+* NPM packages
--- liri.js
+++ liri.js
@@ -8,9 +8,134 @@ var axios = require("axios");
 var moment = require("moment");
  
 var fs = require("fs");
-var query = process.argv[3];
+[node,file,cmd,...query] = process.argv;
+  
+var option = cmd;
+ 
+switch (option) {
+    case "movie-this":
+        movieThis(query);
+        break;
+    case "spotify-this-song":
+        /*if(!query)
+        {
+            query = "The Sign Ace";
+        }*/
+        spotifyCall(query);
+        break;
+    case "concert-this":
+        concertThis(query);
+        break;
+    default:
+        // 1- read file
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            // 2-retrieve content & parse string
+            var data = data.split(",");
+            var thatWay = data[1];
+            if (error) {
+                return console.log(error);
+            }
+            // 3-call function 
+            spotifyCall(thatWay);
+        })
 
- 
-var option = process.argv[2];
- 
-var spotify = new Spotify(keys.spotify);
+}
+
+
+function spotifyCall(songName) {
+    if (songName.length == 0) {
+        songName = ["The Sign Ace"];
+   }
+   
+    var spotify = new Spotify(keys.spotify);
+     
+
+    spotify.search({ type: 'track,artist', query: songName, limit: 5}, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+    var response = data.tracks.items; 
+    for(var i = 0; i < response.length; i++)
+    {
+        var artist = response[i].artists;
+        for(var j = 0; j < artist.length; j++)
+        {
+            console.log(artist[j].name);
+            console.log("\n");
+        }
+        console.log(response[i].name);
+        console.log("\n");
+        var preview_url = response[i].preview_url;
+        if(!response[i].preview_url)
+        {
+            preview_url = "url not found";
+        }
+        console.log(preview_url);
+        console.log("\n");
+        console.log(response[i].album.name);
+        console.log("\n-----------------------");
+    }
+    
+    /*var song = data.tracks.items[0];
+    
+    console.log(song.artists[i].name);
+
+    console.log(song.name);
+
+    console.log(song.preview_url);
+
+    console.log(song.album.name);*/
+
+    });
+}
+
+
+function movieThis(movieName) {
+    if (movieName.length == 0) {
+        movieName = ["Mr. Nobody"];
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+   
+    axios.get(queryUrl).then(
+        function (response) {
+            
+            console.log(response.data.Title)
+            console.log(response.data.Year)
+            console.log(response.data.Country)
+            console.log(response.data.Language)
+            console.log(response.data.Plot)
+            console.log(response.data.Actors)
+
+        }
+    );
+}
+
+
+
+/*function concertThis(artist) {
+    var bandsQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    axios.get(bandsQueryUrl).then(
+        function (response) {
+            console.log(response.data);
+            //console.log("_Upcoming Events_");
+            //console.log("Artist: " + artist + "\nVenue: " + response.data[0].Venue + "\nLocation: " + response.data[0].venue.country + "\nDate: " + response.data[0].datatime + "\nRock on dude!");
+        });
+}*/
+
+
+function concertThis(artist) {
+   
+    var bandsQueryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    
+    axios.get(bandsQueryUrl).then(
+        function (response) {
+            //console.log(response.data);
+            //console.log("\n_Movie Info_" + "\nTitle: " + response.data.Title + "\nRelease Year: " + response.data.Year + "\nRating: " + response.data.Rated + "\nRelease Country: " + response.data.Country + "\nLanguage: " + response.data.Language + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors + "\n" + "\n Love this one!");
+            console.log(response.data[0].venue.name);
+            console.log(moment(response.data[0].datetime).format("MM/DD/YYYY"));
+            console.log(response.data[0].venue.country);
+            console.log(response.data[0].venue.region);
+            console.log(response.data[0].venue.city);
+        }
+    );
+}
